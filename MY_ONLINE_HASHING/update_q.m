c56c86e11b731a3749f0e7d053eccbee673387bb
z97119 <116488351+z97119@users.noreply.github.com>
2022-11-25 16:50:31 +0800

    Update update_q.m

--- MY_ONLINE_HASHING/update_q.m
+++ MY_ONLINE_HASHING/update_q.m
@@ -1,7 +1,5 @@
 function [q,qlabel] = update_q(q,k,Xs_t,ls_t,qlabel)
-%从新到达的流数据中随机更新查询池q
-
-%index = randperm(100,20);
+%Update the query pool%
 n_t = size(Xs_t,2);
 for i = 1:n_t
     d = randi(i+k);
